2e7eedbbe338d09adf5a7b2d44859c155f57b334
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2023-08-07 17:05:03 +0200

    Connecting with WeatherAPI

--- app/src/main/java/com/learning/forecastweathermmvmapp/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/learning/forecastweathermmvmapp/ui/weather/current/CurrentWeatherFragment.kt
@@ -7,6 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.learning.forecastweathermmvmapp.R
+import com.learning.forecastweathermmvmapp.data.WeatherApiService
+import com.learning.forecastweathermmvmapp.databinding.FragmentCurrentWeatherBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -16,17 +21,33 @@ class CurrentWeatherFragment : Fragment() {
 
     private lateinit var viewModel: CurrentWeatherViewModel
 
+    private var _binding: FragmentCurrentWeatherBinding? = null
+    private val binding
+        get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_current_weather, container, false)
+        _binding = FragmentCurrentWeatherBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = WeatherApiService.invoke()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("Zgierz", "pl").await()
+            binding.textView.text = currentWeatherResponse.toString()
+        }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
