082b400a08e8e22416feeb738875cc70e2068afa
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2023-09-10 20:04:37 +0200

    Lesson13: Creating RecyclerView/ fix needed

---
+++ app/src/main/java/com/learning/forecastweathermmvmapp/ui/base/WeatherViewModel.kt
@@ -0,0 +1,22 @@
+package com.learning.forecastweathermmvmapp.ui.base
+
+import androidx.lifecycle.ViewModel
+import com.learning.forecastweathermmvmapp.data.provider.UnitProvider
+import com.learning.forecastweathermmvmapp.data.repository.ForecastRepository
+import com.learning.forecastweathermmvmapp.internal.UnitSystem
+import com.learning.forecastweathermmvmapp.internal.lazyDeferred
+
+abstract class WeatherViewModel(
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
+) : ViewModel() {
+
+    private val unitSystem = unitProvider.getUnitSystem()
+
+    val isMetricUnit: Boolean
+        get() = unitSystem == UnitSystem.METRIC
+
+    val weatherLocation by lazyDeferred {
+        forecastRepository.getWeatherLocation()
+    }
+}
